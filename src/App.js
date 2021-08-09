cb0ec715256d3c20ab566818fad37e9aa579a32e
chaodonghu <dojostan@gmail.com>
2021-08-08 22:07:09 -0400

    add initial axios call to github graph ql endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+const TITLE = "React GraphQL GitHub Client";
 
-function App() {
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+const App = () => {
+  const [path, setPath] = useState(
+    "the-road-to-learn-react/the-road-to-learn-react"
+  );
+  const [organization, setOrganization] = useState(null);
+  const [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+    axiosGitHubGraphQL.post("", { query: GET_ORGANIZATION }).then((result) => {
+      setOrganization(result.data.data.organization);
+      setErrors(result.data.errors);
+    });
+  }, []);
+
+  const handleOnChange = useCallback((event) => {
+    setPath(event.target.value);
+  }, []);
+
+  const handleOnSubmit = useCallback((event) => {
+    event.preventDefault();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{TITLE}</h1>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="url">Show open issues for https://github.com</label>
+        <input
+          id="url"
+          type="text"
+          onChange={handleOnChange}
+          style={{ width: "300px" }}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <hr />
     </div>
   );
-}
+};
 
 export default App;
